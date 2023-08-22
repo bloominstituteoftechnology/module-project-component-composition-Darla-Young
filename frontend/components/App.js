52a514e9dbb64ea5de1488051b5a5ba6744078da
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-08-22 11:14:30 -0500

    complete

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -2,52 +2,29 @@ import React from 'react'
 import {useState, useEffect} from 'react';
 import Axios from 'axios';
 
-function App() {
-  // const [title, setTitle] = useState();
-  // const [date, setDate] = useState();
-  // const [explanation, setExplanation] = useState();
-  const [picture, setPicture] = useState({title: '', date: '', explanation: '', picture: ''});
-  const getPic = async () => {
-    const res = await Axios.get('https://api.nasa.gov/planetary/apod?sol=1000&api_key=DEMO_KEY');
-    const pic = {
-      title: res.data.title,
-      date: res.data.date,
-      explanation: res.data.explanation,
-      picture: res.data.hdurl
-    }
-    setPicture(pic);
+function App () {
+  const [picture, setPicture] = useState({title: '', date: '', explanation: '', hdurl: ''});
+
+  const getPic = () => {
+    Axios
+      .get ('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=1')
+      .then (response => {
+        console.log (response.data[0]); /* {date, explanation, hdurl, media_type, service_version, title, url} */
+        if (response.data[0].media_type != 'image') {getPic()}
+        else {setPicture(response.data[0])}
+      })
+      .catch(error => console.log(error));
   }
 
-  useEffect(()=> {getPic, getPic()}, []);
-
-
-  // const changePic = () => {
-  //   Axios
-  //     .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-  //     .then(response => {
-  //       console.log(response.data); /* {date, explanation, hdurl, media_type, service_version, title, url} */
-  //       setTitle(response.data.title); 
-  //       setDate(response.data.date); 
-  //       setExplanation(response.data.explanation); 
-  //       setPicture(response.data.hdurl);
-  //     })
-  //     .catch(error => console.log(error));
-  // }
-
-  // useEffect(changePic, []);
-
-  const style = {
-    fontSize: '1.5 rem',
-    margin: '0.5 rem'
-  }
+  useEffect(getPic, []);
 
   return (
     <div className='nasa'>
       <h1>{picture.title}</h1>
       <h2>{picture.date}</h2>
       <p>{picture.explanation}</p>
-      <img src={picture.picture} alt={'image of ' + picture.title}/>
-      <button onClick={getPic} style={style}>New Random Photo</button>
+      <img src={picture.hdurl} alt={'image of ' + picture.title}/>
+      <button onClick={getPic}>New Random Photo</button>
     </div>
   )
 }
