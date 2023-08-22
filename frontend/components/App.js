156340111b8866e2b1c17884a8162bdbdab45395
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-08-22 09:56:36 -0500

    working on random image

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,25 +3,38 @@ import {useState, useEffect} from 'react';
 import Axios from 'axios';
 
 function App() {
-  const [title, setTitle] = useState();
-  const [date, setDate] = useState();
-  const [explanation, setExplanation] = useState();
-  const [picture, setPicture] = useState();
-
-  const changePic = () => {
-    Axios
-      .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-      .then(response => {
-        console.log(response.data); /* {date, explanation, hdurl, media_type, service_version, title, url} */
-        setTitle(response.data.title); 
-        setDate(response.data.date); 
-        setExplanation(response.data.explanation); 
-        setPicture(response.data.hdurl);
-      })
-      .catch(error => console.log(error));
+  // const [title, setTitle] = useState();
+  // const [date, setDate] = useState();
+  // const [explanation, setExplanation] = useState();
+  const [picture, setPicture] = useState({title: '', date: '', explanation: '', picture: ''});
+  const getPic = async () => {
+    const res = await Axios.get('https://api.nasa.gov/planetary/apod?sol=1000&api_key=DEMO_KEY');
+    const pic = {
+      title: res.data.title,
+      date: res.data.date,
+      explanation: res.data.explanation,
+      picture: res.data.hdurl
+    }
+    setPicture(pic);
   }
 
-  useEffect(changePic, []);
+  useEffect(()=> {getPic, getPic()}, []);
+
+
+  // const changePic = () => {
+  //   Axios
+  //     .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  //     .then(response => {
+  //       console.log(response.data); /* {date, explanation, hdurl, media_type, service_version, title, url} */
+  //       setTitle(response.data.title); 
+  //       setDate(response.data.date); 
+  //       setExplanation(response.data.explanation); 
+  //       setPicture(response.data.hdurl);
+  //     })
+  //     .catch(error => console.log(error));
+  // }
+
+  // useEffect(changePic, []);
 
   const style = {
     fontSize: '1.5 rem',
@@ -30,11 +43,11 @@ function App() {
 
   return (
     <div className='nasa'>
-      <h1>{title}</h1>
-      <h2>{date}</h2>
-      <p>{explanation}</p>
-      <img src={picture} alt={'image of ' + title}/>
-      <button onClick={changePic} style={style}>New Random Photo</button>
+      <h1>{picture.title}</h1>
+      <h2>{picture.date}</h2>
+      <p>{picture.explanation}</p>
+      <img src={picture.picture} alt={'image of ' + picture.title}/>
+      <button onClick={getPic} style={style}>New Random Photo</button>
     </div>
   )
 }
